447edd97acbad914745472bcbb75f78f17eec966
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-12-04 21:47:50 +0530

    Added basic recursive program

---
+++ recursion/src/BasicRecursion.java
@@ -0,0 +1,19 @@
+public class BasicRecursion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello in the world of recursion !!");
+        System.out.println("Calling a recursive function");
+        printName(0);
+        System.out.println("Control returned from function to main !!");
+    }
+
+    public static void printName(int count) {
+
+        //Base case
+        if (count > 5) return;
+
+        System.out.println("Hello Ujjwal !!");
+        count++;
+        printName(count);
+    }
+}
