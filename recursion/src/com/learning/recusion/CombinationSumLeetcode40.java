046a6ab6c834f52382304937825782bfb58d4eeb
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-02-13 08:44:42 +0530

    added combination sum 2 recursive solution

--- recursion/src/com/learning/recusion/CombinationSumLeetcode40.java
+++ recursion/src/com/learning/recusion/CombinationSumLeetcode40.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class CombinationSumLeetcode40 {
     public static void main(String[] args) {
-        List<Integer> arr = new ArrayList<>(Arrays.asList(10,1,2,7,6,1,5));
+        List<Integer> arr = new ArrayList<>(Arrays.asList(10, 1, 2, 7, 6, 1, 5));
         int target = 8;
         Set<List<Integer>> ans = new HashSet<>();
 
@@ -19,19 +19,19 @@ public class CombinationSumLeetcode40 {
     private static void getCombinationSum(int index, List<Integer> arr, List<Integer> tmpAns, Set<List<Integer>> ans, int target) {
 
 
-        if(target==0){
+        if (target == 0) {
             ans.add(new ArrayList<>(tmpAns));
             return;
         }
 
         //pick with duplicates avoid
-        for(int i = index; i < arr.size(); i++){
-            if(i > index && Objects.equals(arr.get(i), arr.get(i - 1))) continue;
+        for (int i = index; i < arr.size(); i++) {
+            if (i > index && Objects.equals(arr.get(i), arr.get(i - 1))) continue;
 
-            if(arr.get(i) > target) break;
+            if (arr.get(i) > target) break;
 
             tmpAns.add(arr.get(i));
-            getCombinationSum(i+1, arr, tmpAns, ans, target-arr.get(i));
+            getCombinationSum(i + 1, arr, tmpAns, ans, target - arr.get(i));
             tmpAns.remove(arr.get(i));
         }
 
