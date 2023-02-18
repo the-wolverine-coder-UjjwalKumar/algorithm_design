963ace31b80c39c954b082192ca5796117c6de6a
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-02-18 20:29:24 +0530

    added tree node class

---
+++ recursion/src/com/learning/tree/Node.java
@@ -0,0 +1,12 @@
+package com.learning.tree;
+
+public class Node {
+    public int data;
+    public Node left, right;
+
+    public Node(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
