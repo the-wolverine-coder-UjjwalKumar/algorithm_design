ff3c68cf1c45c7de7ce61057ad06e845094061c9
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-03-04 19:05:50 +0530

    added CostofRemoveNumbers.java

---
+++ recursion/src/com/learning/arrays/CostofRemoveNumbers.java
@@ -0,0 +1,23 @@
+package com.learning.arrays;
+
+public class CostofRemoveNumbers {
+    public static void main(String[] args) {
+        int[] arr = {1, 17, 18, 17, 1};
+        int[] arr2 = {3, 2, 2, 1};
+        int[] arr3 = {2, 2, 2, 3};
+        long m = 1000000007;
+
+        long ans = getRemovalCost(arr3, m);
+        System.out.println(ans);
+    }
+
+    private static long getRemovalCost(int[] arr, long m) {
+        if (arr != null && arr.length <= 2) return 0;
+
+        long ans = 0;
+        for (int i = 1; i <= arr.length - 2; i++) {
+            ans += (Math.min(arr[i - 1], arr[i + 1])) % m;
+        }
+        return ans % m;
+    }
+}
