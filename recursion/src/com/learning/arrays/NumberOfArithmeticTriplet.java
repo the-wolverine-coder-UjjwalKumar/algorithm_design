4a9234485ee5ca05b637f055786df2dd31721860
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-03-26 15:39:55 +0530

    Added NumberOfArithmeticTriplet

---
+++ recursion/src/com/learning/arrays/NumberOfArithmeticTriplet.java
@@ -0,0 +1,23 @@
+package com.learning.arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NumberOfArithmeticTriplet {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        Set<Integer> set = new HashSet<>();
+        for( int num : nums){
+            set.add(num);
+        }
+        int ans = 0;
+        for( int num : nums){
+            if(( set.contains(num + diff ) && set.contains(num + 2 * diff) ) ){
+                ans++;
+
+            }
+        }
+
+        return ans;
+
+    }
+}
